df3d64702e251033b1fc21da6ddab2b9aeb746f3
NearSharp <znathanmz@gmail.com>
2024-04-04 14:43:23 -0500

    initial commit

--- src/CreateController.py
+++ src/CreateController.py
@@ -1,4 +1,13 @@
 #PRESS ALT + SHIFT + M
 import maya.cmds as mc
 
-print("hello!! Maya")
+from PySide2.QtWidgets import QWidget
+
+class CreateControllerWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("create IKFK Limb")
+        
+
+controllerWidget = CreateControllerWidget()
+controllerWidget.show()        
